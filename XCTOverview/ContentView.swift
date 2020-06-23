155838968bc7470e2e52dd09be1a07ea25702e3e
Gio Lodi <giovanni.lodi42@gmail.com>
2020-06-23 22:13:43 +1000

    Initial Commit

---
+++ XCTOverview/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  XCTOverview
+//
+//  Created by Gio on 23/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ XCTOverview/XCTOverviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  XCTOverviewApp.swift
+//  XCTOverview
+//
+//  Created by Gio on 23/6/20.
+//
+
+import SwiftUI
+
+@main
+struct XCTOverviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
