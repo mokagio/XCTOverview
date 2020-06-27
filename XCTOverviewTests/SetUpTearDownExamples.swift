98eea8c76bedb2311c70a85bb147e0f5adcd9ee9
Gio Lodi <giovanni.lodi42@gmail.com>
2020-06-27 12:58:35 +1000

    Use clearer name and comments in examples of test lifecycle

--- XCTOverviewTests/SetUpTearDownExamples.swift
+++ XCTOverviewTests/SetUpTearDownExamples.swift
@@ -3,16 +3,16 @@ import XCTest
 
 private var statefulValue: Int = 0
 
-class XCTOverviewTests: XCTestCase {
+class SetUpTearDownExamples: XCTestCase {
 
     // There is no "with error" version of this, yet.
     override class func setUp() {
-        print("This run before all tests")
+        print("This runs before all tests")
         statefulValue += 1
     }
 
     override func setUpWithError() throws {
-        print("This run before each test")
+        print("This runs before each test")
         statefulValue += 1
     }
 
@@ -25,6 +25,14 @@ class XCTOverviewTests: XCTestCase {
         print("This runs after each test")
     }
 
+    // Using A and B so we can control in which order these tests run, because order matters here.
+    //
+    // Note that if we turn on the randomized test execution option in the scheme, these tests might
+    // fail. This is a good example of why you shouldn't depend on global state and execution order
+    // in your tests.
+    //
+    // TODO: Move this test in a dedicated target or scheme so that we can keep using it as an
+    // example of the drawbacks of depending on state in the tests.
     func testA() throws {
         XCTAssertEqual(statefulValue, 2)
     }
